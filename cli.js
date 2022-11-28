05ad6ca909db9ff38b9ad26d1d4c1f710099e609
Tao Huang <89457786+TaoHuang0@users.noreply.github.com>
2022-11-28 13:42:56 -0500

    Update cli.js

--- cli.js
+++ cli.js
@@ -14,12 +14,12 @@ if(args.t){
 
 if(args.h){
 	console.log(`Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE
-    -h            Show this help message and exit.
-    -n, -s        Latitude: N positive; S negative.
-    -e, -w        Longitude: E positive; W negative.
-    -z            Time zone: uses tz.guess() from moment-timezone by default.
-    -d 0-6        Day to retrieve weather: 0 is today; defaults to 1.
-    -j            Echo pretty JSON from open-meteo API and exit.
+			    -h            Show this help message and exit.
+			    -n, -s        Latitude: N positive; S negative.
+			    -e, -w        Longitude: E positive; W negative.
+			    -z            Time zone: uses tz.guess() from moment-timezone by default.
+			    -d 0-6        Day to retrieve weather: 0 is today; defaults to 1.
+			    -j            Echo pretty JSON from open-meteo API and exit.
 	`)
 }else{
 	let latitude = '35';
