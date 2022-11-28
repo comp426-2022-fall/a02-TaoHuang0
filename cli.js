c59f9d8387f7a58f8781af14dc7f491cc035d4ec
Tao Huang <89457786+TaoHuang0@users.noreply.github.com>
2022-11-28 18:32:00 -0500

    Update cli.js

--- cli.js
+++ cli.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 import minimist from 'minimist'
-import moment from 'moment-timezone';
+import moment from 'moment-timezone'
 import node_fetch from 'node-fetch';
 
 const args = minimist(process.argv.slice(2));
